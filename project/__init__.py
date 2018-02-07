1b8ce09cb70ccf7ebd4f5430c3e8d29aed476e6a
Dayne Beck <daynebeck28@gmail.com>
2018-02-07 16:35:21 -0600

    Instances folder added

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+
+################
+#### config ####
+################
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
+
 from . import views
